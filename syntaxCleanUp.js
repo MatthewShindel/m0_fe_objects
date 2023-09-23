2899adc4edd45e477b8d67295a0cc143183d22e7
MatthewShindel <matthew@shindel.com>
2023-09-23 12:49:10 -0400

    clean syntaxCleanUp.js

--- syntaxCleanUp.js
+++ syntaxCleanUp.js
@@ -1,22 +1,33 @@
-var foods = {apples: 23, 
+var foods = {
+  apples: 23, 
   grapes: 507, 
-  eggs: 48}
+  eggs: 48
+};
 
-var person = { height: "6 ft", weight: "160 lbs" }
+// not sure if I should be adding comments or not, but here goes
+// Moved the body of foods into a seperate line, move ending } to a new line. added  to end;
 
-var post = { imageSrc: "./images/beach.png",
+var person = { 
+  height: "6 ft",
+  weight: "160 lbs" 
+};
+// Seperated key-value pairs into sepreate lines,  move ending } to a new line. added  to end;
+
+var post = {
+  imageSrc: "./images/beach.png",
+  caption: "At the beach with my besties", timestamp: "4:37 PM August 13, 2019",
+  number_likes: 0,
+  comments: [] 
+};
+// standarized the indentation of the key-value pairs, more inital key-value pair imageSrc to new line
+// move ending } to a new line. added  to end;
 
 
-   caption: "At the beach with my besties", timestamp: "4:37 PM August 13, 2019",
-        number_likes: 0,
-comments: [] }
-
-
-var post2 =
-{
+var post2 = {
       imageSrc: "./images/holiday-party.png",
       caption: "What a great holiday party omg",
       timestamp: "11:37 PM December 31, 2019",
       numberLikes: 13,
       comments: []
-}
+};
+// moved first { up to same line as decleration, added  to end;
